f3e5d654c294b2db6002aaecd6e8bd99128f922f
Jayin Ton <tonjayin@gmail.com>
2014-04-19 17:13:50 +0800

    support paging and chinese file name

--- diaries/make.py
+++ diaries/make.py
@@ -1,36 +1,56 @@
 #encoding:utf-8
 #to do:
-#support Chinese File name yet...
+#support Chinese File name yet...  [ok]
 #support paging yet
-#support sort the diaries by time
+#support sort the diaries by time [ok]
 __author__ = 'Jayin Ton'
 
-import os, re, json
+import os, re, json, copy
 
-pageCount = 10;
+
+#config
+limit = 10
 ignore = [
     'README.md',
     '.+\.json',
     '.+\.py'
 ]
 
-def sort(files):
-    print files.sort(reverse=True)
+
+def generation(page, list):
+    with open(str(page) + ".json", mode="w") as f:
+        f.write(json.dumps({'result': ','.join(list).decode('gbk').split(',')}))
 
 
 def main():
     l = list(os.listdir(os.getcwd()))
+    raw = copy.deepcopy(l)
     for ig in ignore:
         pattern = re.compile(ig, re.IGNORECASE)
         for t in l:
-            if (re.findall(pattern, t)):
-                l.remove(t)
-    l.sort(reverse=False)
-    with open("1.json", mode="w") as f:
-        f.write(json.dumps({'result': l}))
+            if re.findall(pattern, t):
+                raw.remove(t)
+    raw.sort(reverse=False)
+    # with open("1.json", mode="w") as f:
+    #     f.write(json.dumps({'result': [','.join(l).decode('gbk').split(',')]}))
+    size = len(raw)
+    print size
+    page = 1
+    while page * limit <= size:
+        print page, '--> ', raw[(page - 1) * limit:page * limit]
+        generation(page, raw[(page - 1) * limit:page * limit])
+        page += 1
+    print page, '--> ', raw[(page - 1) * limit:]
+    generation(page, raw[(page - 1) * limit:])
+
     print("Build finished")
-    print l
+
+
+def cleanUp():
+    pass
 
 
 if __name__ == "__main__":
+    cleanUp()
     main()
+
